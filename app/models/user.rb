41989ac5b65a5cab72233ad74168a53b40fc9967
thanhlt-1007 <le.tan.thanh@sun-asterisk.com>
2019-07-20 18:52:10 +0700

    add_User.create_random_and_user#update_random

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,11 @@
 class User < ApplicationRecord
+  def update_random
+    update_attributes name: FFaker::Name.name , email: FFaker::Internet.email   
+  end
+
+  class << self
+    def create_random
+      User.create name: FFaker::Name.name , email: FFaker::Internet.email
+    end
+  end
 end
