50a66783cfe2780b5f4202f1ff5f2e6df41e19d0
thanhlt-1007 <le.tan.thanh@sun-asterisk.com>
2019-07-20 20:14:07 +0700

    handle_asynchronously

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,9 +3,13 @@ class User < ApplicationRecord
     update_attributes name: FFaker::Name.name , email: FFaker::Internet.email   
   end
 
+  handle_asynchronously :update_random
+
   class << self
     def create_random
       User.create name: FFaker::Name.name , email: FFaker::Internet.email
     end
+
+    handle_asynchronously :create_random
   end
 end
